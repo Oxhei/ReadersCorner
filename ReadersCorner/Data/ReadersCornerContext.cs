bb876ed65d30f42c49290f8cd1da647846725715
Richard Ochei <84982365+Oxhei@users.noreply.github.com>
2024-01-26 06:37:12 +0000

    added Register

---
+++ ReadersCorner/Data/ReadersCornerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReadersCorner.Models;
+
+namespace ReadersCorner.Data
+{
+    public class ReadersCornerContext : DbContext
+    {
+        public ReadersCornerContext (DbContextOptions<ReadersCornerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReadersCorner.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ ReadersCorner/Pages/Register/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ReadersCorner.Data;
+using ReadersCorner.Models;
+
+namespace ReadersCorner.Pages.Register
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ReadersCorner.Data.ReadersCornerContext _context;
+
+        public CreateModel(ReadersCorner.Data.ReadersCornerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.User == null || User == null)
+            {
+                return Page();
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
